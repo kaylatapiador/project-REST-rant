e9b71779d98435722fabeb7e3f6c15727a0fadbd
kayla tapiador <kaylatapiador@yahoo.com>
2024-03-14 21:25:07 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404(){
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/rabbit.jpg" alt="Rabbit"/>
+                    <div>
+                        Photo by <a href="https://unsplash.com/@cadop?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mathew Schwartz</a> on <a href="https://unsplash.com/photos/a-small-rabbit-is-sitting-in-the-grass-7gl8ffzOwZU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,13 @@ function home(){
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/Grilled-Meat-and-Veggies.jpg" alt="Grilled Meat and Veggies"/>
+                    <div>
+                       Photo by <a href="https://unsplash.com/@victoriakosmo?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Victoria Shes</a> on <a href="https://unsplash.com/photos/grilled-meat-and-vegetable-on-the-table-UC0HZdUitWY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
