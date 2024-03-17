3bacc51aee92e88d55149d8a172adc94b7e0c799
kayla tapiador <kaylatapiador@yahoo.com>
2024-03-17 13:25:53 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -8,6 +8,13 @@ function Def (html){
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"/>
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/places">Places</a></li>
+                    <li><a href="/places/new">Add Place</a></li>
+                </ul>
+            </nav>
             <body>
                 {html.children}
             </body>
--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home(){
     return(
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>Rest-Rant</h1>
                 <div>
                     <img src="/images/Grilled-Meat-and-Veggies.jpg" alt="Grilled Meat and Veggies"/>
                     <div>
