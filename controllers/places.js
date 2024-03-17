d5c146e3fb3457accbcea93809d6d9574c359802
kayla tapiador <kaylatapiador@yahoo.com>
2024-03-17 13:46:07 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -25,4 +25,18 @@ router.post('/', (req,res)=>{
   res.redirect('/places')
 })
 
+//Show route
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place:places[id] })
+  }
+})
+
 module.exports = router
