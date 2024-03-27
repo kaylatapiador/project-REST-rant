cc294c4aff5f0f7389b58fedfb5126d38b31289c
kayla tapiador <kaylatapiador@yahoo.com>
2024-03-26 19:03:25 -0700

    part 9 completed

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,25 +5,40 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1> { data.place.name} </h1>
-            <img src={data.place.pic} alt={data.place.name}/>
-
-            <h2>Rating</h2>
-            <h3>Not yet rated!</h3>
-
-            <h2>Description</h2>
-            <h3>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</h3>
+            <div className = "row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name}/>
+                <h3>Located in {data.place.city}, {data.place.state}</h3>
+              </div>
+            </div>
+            
+            <div className='col-sm-6'>
+              <h1>{data.place.name}</h1>
+              <h2>Rating</h2>
+              <h3>Not Rated</h3>
+              <h2>Description</h2>
+              <h3>
+                {data.place.showEstablished()}
+              </h3>
+              <h4>
+                Serving {data.place.cuisines}
+              </h4>
+            </div>
 
             <h2>Comments</h2>
             <h3>No comments yet!</h3>
 
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning" class="bi bi-pencil-square"> Edit</a>     
 
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-              <button type="submit" className="btn btn-danger">
-                 Delete
-              </button>
-            </form> 
+            <div className='btn-group'>
+              <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                  Edit
+              </a>
+              <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                <button type="submit" className="btn btn-danger">
+                  Delete
+                </button>
+              </form>
+            </div>
 
 
           </main>
