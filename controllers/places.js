79722436d04b05e0b048961126610ae22064499b
kayla tapiador <kaylatapiador@yahoo.com>
2024-03-28 22:20:31 -0700

    comments work thru posting. need to fix check box

--- controllers/places.js
+++ controllers/places.js
@@ -58,25 +58,31 @@ router.get('/:id/edit', (req, res) => {
 })
 
 router.post('/:id/comment', (req, res) => {
-  console.log(req.body)
+  console.log('post comment', req.body)
+  if (req.body.author === '') { req.body.author = undefined }
+  req.body.rant = req.body.rant ? true : false
   db.Place.findById(req.params.id)
-  .then(place => {
-      db.Comment.create(req.body)
-      .then(comment => {
-          place.comments.push(comment.id)
-          place.save()
-          .then(() => {
-              res.redirect(`/places/${req.params.id}`)
-          })
+      .then(place => {
+          db.Comment.create(req.body)
+              .then(comment => {
+                  place.comments.push(comment.id)
+                  place.save()
+                      .then(() => {
+                          res.redirect(`/places/${req.params.id}`)
+                      })
+                      .catch(err => {
+                          res.render('error404')
+                      })
+              })
+              .catch(err => {
+                  res.render('error404')
+              })
       })
       .catch(err => {
           res.render('error404')
       })
-  })
-  .catch(err => {
-      res.render('error404')
-  })
 })
+
 
 
 
